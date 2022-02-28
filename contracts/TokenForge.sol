be9b7bdd00b596ca4a5b444112a910a8f7b472fa
Jeff Prestes <jeffprestes@gmail.com>
2022-02-28 10:15:08 -0300

    Remove unnecessary contracts

--- contracts/TokenForge.sol
+++ contracts/TokenForge.sol
@@ -11,6 +11,34 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+abstract contract ERCGovernance {
+
+    event GrantOperator(address indexed account);
+    event RevokeOperator(address indexed account);
+
+    function changeOwner(address _newOwner) public onlyOwner returns (bool)     {
+        require(_newOwner != address(0), "You must inform a valid address");
+        owner = payable(_newOwner);
+        return true;
+    }
+
+    function grantOperator(address _account) public onlyOwner   {
+        operators[_account] = true;
+        emit GrantOperator(_account);
+    }
+
+    function revokeOperator(address _account) public onlyOwner  {
+        operators[_account] = false;
+        emit RevokeOperator(_account);
+    }
+
+    constructor()   {
+        owner = msg.sender;
+        grantOperator(msg.sender);
+    }
+
+}
+
 abstract contract ERC721URIStorage is ERC721 {
     using Strings for uint256;
 
@@ -109,7 +137,7 @@ contract Token721 is ERC721URIStorage{
 
 }
 
-contract Token1155 is ERC1155{
+contract Token1155 is ERC1155 {
     address payable public owner;
     address public thiscontract;
 
@@ -149,14 +177,22 @@ contract Token1155 is ERC1155{
 }
 
 
-contract TokenERC20 is ERC20, ERC20Burnable,ERC20Pausable, ERC20Permit { 
-    address payable public owner; 
+contract TokenERC20 is ERC20, ERC20Burnable, ERC20Pausable, ERC20Permit { 
+    
     uint8 public decimal;
     address public thisContract;
 
-
+    address payable public owner; 
     modifier onlyOwner {
         require(msg.sender == owner,"Only owner can call this function.");
+        _;
+    }
+
+    mapping(address => bool) public operators;
+    bytes32 private constant OPERATOR = keccak256(abi.encodePacked("OPERATOR"));
+    
+    modifier onlyOperator() {
+        require(operators[msg.sender],"not operator");
         _;
     }
 
@@ -184,13 +220,10 @@ contract TokenERC20 is ERC20, ERC20Burnable,ERC20Pausable, ERC20Permit {
     }
 
 
-    function changeOwner(address _newOwner) public onlyOwner returns (bool){
-        require(_newOwner != address(0), "You must inform a valid address");
-        owner = payable(_newOwner);
-        return true;
-    }
+    
 
-    function mintToAdress(address mint, uint supply) public onlyOwner{
+    
+    function mintToAdress(address mint, uint supply) public onlyOperator {
         _mint(mint,supply);
     }
 
@@ -202,67 +235,48 @@ contract TokenERC20 is ERC20, ERC20Burnable,ERC20Pausable, ERC20Permit {
 
 
 contract Tokenforge {
-    event GrantOperator(address indexed account);
-    event RevokeOperator(address indexed account);
+    
     event NewERC20(string _name, string _symbol, uint8 _decimal,address indexed _addr);
     event NewERC721(string _name, string symbol, address indexed _addr);
     event NewERC1155(address indexed _addr);
-    enum TokenType{
+    enum TokenType {
         ERC20,
         ERC721,
         ERC1155
     }
 
-    struct DataToken{
+    struct TokenData {
         string name;
         string symbol;
         TokenType token;
         address addrcontract;
     }
 
-    DataToken[] public datas;
+    TokenData[] public tokenGeneratedData;
+    uint public tokensGenerated;
 
     mapping(address => bool) public operators;
-    //aacount => bool
-    address public owner;
-
     bytes32 private constant OPERATOR = keccak256(abi.encodePacked("OPERATOR"));
-
-    modifier onlyOperator(){
+    
+    modifier onlyOperator() {
         require(operators[msg.sender],"not operator");
         _;
     }
 
+    address public owner;
     modifier onlyOwner {
         require(msg.sender == owner,"Only owner can call this function.");
         _;
     }
 
-    constructor()   {
-        owner = msg.sender;
-        grantOperator(msg.sender);
-    }
-
-    function changeOwner(address _newOwner) public onlyOwner returns (bool){
-        require(_newOwner != address(0), "You must inform a valid address");
-        owner = payable(_newOwner);
-        return true;
-    }
-
-    function grantOperator(address _account) public onlyOwner{
-        operators[_account] = true;
-        emit GrantOperator(_account);
-    }
-
-    function revokeOperator(address _account) public onlyOwner{
-        operators[_account] = false;
-        emit RevokeOperator(_account);
-    }
+    
+    
 
     function forgeNewTokenERC20 (string memory _name, string memory _symbol, uint8 _decimal) external onlyOperator {
         TokenERC20 token20 = new TokenERC20(_name,_symbol,_decimal);
         address _addr = token20.addr();
-        datas.push(DataToken({name: _name,symbol:_symbol, token: TokenType.ERC20, addrcontract: _addr}));
+        tokenGeneratedData.push(TokenData({name: _name,symbol:_symbol, token: TokenType.ERC20, addrcontract: _addr}));
+        tokensGenerated++;
         emit NewERC20(_name, _symbol,_decimal, _addr);
     }
 }
